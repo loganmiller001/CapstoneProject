707dcec3378f89f81a64737833e5a9da4254624b
Logan Miller <loganmiller001@live.com>
2018-08-14 11:41:04 -0500

    Also created the company commander model.

---
+++ Capstone/Models/CompanyCommander.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class CompanyCommander
+    {
+        [Key]
+        public int CommanderId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Rank { get; set; }
+
+        public string SocialSecurityNumber { get; set; }
+    }
+}
