6d848fbf6e3f36f87edaf7be24d7fe4590c82672
Logan Miller <loganmiller001@live.com>
2018-08-14 11:43:19 -0500

    Added in ommited DisplayNames for properties in CompanyCommander model.

--- Capstone/Models/CompanyCommander.cs
+++ Capstone/Models/CompanyCommander.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -11,12 +12,16 @@ namespace Capstone.Models
         [Key]
         public int CommanderId { get; set; }
 
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
+        [DisplayName("Rank/Pay Grade")]
         public string Rank { get; set; }
 
+        [DisplayName("Social Security Number")]
         public string SocialSecurityNumber { get; set; }
     }
 }
