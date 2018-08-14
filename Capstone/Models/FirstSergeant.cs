2de17e89daa27b4ef2ff482a7f2d11db6ed2eddd
Logan Miller <loganmiller001@live.com>
2018-08-14 11:38:34 -0500

    Created inital solider and 1st Sergeant models, added properties to both.

---
+++ Capstone/Models/FirstSergeant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class FirstSergeant
+    {
+        [Key]
+        public int FirstSergeantID { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Social Security Number")]
+        public int SocialSecurityNumber { get; set; }
+
+        [DisplayName("Rank/Pay Grade")]
+        public string Rank { get; set; }
+
+
+    }
+}
---
+++ Capstone/Models/Soldier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Soldier
+    {
+        [Key]
+        public int SoldierId { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Rank/Pay Grade")]
+        public string Rank { get; set; }
+
+        [DisplayName("Social Security Number")]
+        public int SocialSecurityNumber { get; set; }
+
+        [DisplayName("DA-31")]
+        public string LeaveForm{get; set;}
+
+        [DisplayName("Leave and Earning Statement")]
+        public string LES { get; set; }
+
+        [DisplayName("Assigned Unit")]
+        public string UnitNumber { get; set; }
+
+        [DisplayName("Division")]
+        public string Division { get; set; }
+
+        [DisplayName("First Line Supervisor")]
+        public string Leadership { get; set; }
+    }
+    
+}
